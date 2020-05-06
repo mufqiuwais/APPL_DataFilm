7fa7d47c5fb1585d4918192c200eb4c8fd4d3416
mufqiuwais <mufqi.uwais.tif418@polban.ac.id>
2020-05-06 19:26:35 +0700

    nambah after

--- src/main/java/aspect/LoggingAspect.java
+++ src/main/java/aspect/LoggingAspect.java
@@ -1,9 +1,18 @@
 package aspect;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class LoggingAspect {
+	@Pointcut("execution(** query.IQuery.create(..))")
+	public void creation() {}
+	
+	@Pointcut("execution(** query.IQuery.update(..))")
+	public void makeUpdate() {}
+	
+	@Pointcut("execution(** query.IQuery.delete(..))")
+	public void deletion() {}
 	
 	@Before("execution(* query.IQuery.*(..))")
 	public void logBeforeAllMethods(JoinPoint joinPoint)
@@ -12,17 +21,79 @@ public class LoggingAspect {
 				joinPoint.getSignature().getName());
 	}
 	
-	@Before("execution(* query.IQuery.create(..))")
-	public void logBeforeCreate(JoinPoint joinPoint)
+	@Before("creation()")
+	public void beforeCreate(JoinPoint joinPoint)
 	{
-		System.out.println("Logging Aspect Before Create" +
+		System.out.println("Creating A New Film... ->" +
 				joinPoint.getSignature().getName());
 	}
 	
-	@Before("execution(* query.IQuery.deleteAll(..))")
-	public void logBeforeDeleteAll(JoinPoint joinPoint)
+	@AfterReturning("creation()")
+	public void afterCreate(JoinPoint joinPoint)
 	{
-		System.out.println("Logging Aspect Before Delete All" +
+		System.out.println("New Film Created ->" +
 				joinPoint.getSignature().getName());
+	}
+	
+	@AfterThrowing("creation()")
+	public void afterCreateButFail(JoinPoint joinPoint)
+	{
+		System.out.println("Fail to Create A New Film");
+	}
+	
+	@Before("makeUpdate()")
+	public void beforeUpdate(JoinPoint joinPoint)
+	{
+		System.out.println("Updating The Film... ->" +
+				joinPoint.getSignature().getName());
+	}
+	
+	@AfterReturning("makeUpdate()")
+	public void afterUpdate(JoinPoint joinPoint)
+	{
+		System.out.println("The Film Updated ->" +
+				joinPoint.getSignature().getName());
+	}
+	
+	@AfterThrowing("makeUpdate()")
+	public void afterUpdateButFail(JoinPoint joinPoint)
+	{
+		System.out.println("Fail to Update The Film");
+	}
+	
+	@Before("deletion()")
+	public void beforeDelete(JoinPoint joinPoint)
+	{
+		System.out.println("Deleting The Film... ->" +
+				joinPoint.getSignature().getName());
+	}
+	
+	@AfterReturning("deletion()")
+	public void afterDelete(JoinPoint joinPoint)
+	{
+		System.out.println("The Film Deleted ->" +
+				joinPoint.getSignature().getName());
+	}
+	
+	@AfterThrowing("deletion()")
+	public void afterDeleteButFail(JoinPoint joinPoint)
+	{
+		System.out.println("Fail to Delete The Film");
+	}
+	
+	@Around("execution(* query.IQuery.deleteAll(..))")
+	public void logBeforeDeleteAll(ProceedingJoinPoint jp)
+	{
+		try {
+			System.out.println("Preparing to Delete All Film... -> ");
+			System.out.println("You'll be unable to see them again... ");
+			System.out.println("Ooowh, I think you've choosen the wrong option... ");
+			System.out.println("Come On DUDE... ");
+		}
+		catch (Throwable e) {
+			// TODO: handle exception
+			System.out.println("Fail to Delete All Film");
+			System.out.println("So the force still be with you huh...");
+		}
 	}
 }
